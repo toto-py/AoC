239dffd998e21e8e6ee240f0c5f9eb698eb31b64
toto-py <193012238+toto-py@users.noreply.github.com>
2025-01-05 00:42:16 +0100

    Changed day01part1 and input files

--- day01/day01part1.py
+++ day01/day01part1.py
@@ -1,22 +1,19 @@
-input_file = open("input")
+"""Advent of Code - Day 1 Part 1"""
 
-left_list = list()
-right_list = list()
-
-for line in input_file:
-	line_items = line.split()
-	left_list.append(int(line_items[0]))
-	right_list.append(int(line_items[1]))
+with open("day01/input", "r", encoding="utf8") as file:
+    left_list = []
+    right_list = []
+    for line in file:
+        line_items = [int(x) for x in line.split()]
+        left_list.append(line_items[0])
+        right_list.append(line_items[1])
 
 left_list.sort()
 right_list.sort()
 
-distance = 0
+distance: int = 0
 
 for x in range(len(left_list)):
-	distance = distance + abs(left_list[x] - right_list[x])
+    distance = distance + abs(left_list[x] - right_list[x])
 
-print(distance)
-
-input_file.close()
-
+print(f"The total distance between the lists is {distance}")
